a0cc9f5bc1e5a91215afdba54ae0dab2de4b10f8
Rrsszxx Official <103621464+Rrsszxx98@users.noreply.github.com>
2022-05-14 16:05:32 +0700

    Add files via upload

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -8,7 +8,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   }, {
     "displayName": "ʀʀꜱꜱᴢxx ʙᴏᴛ",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ʀʀꜱꜱᴢxx ʙᴏᴛ\nitem1.TEL;waid=62895401928948:62895401928948\nitem1.X-ABLabel:📍 Bot Whatsapp\nitem2.EMAIL;type=INTERNET:rrsszxx98@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://api-invibot.herokuapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Japan;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
-    }]
+    },{
+      "displayName": "𝙾𝚠𝚗𝚎𝚛 𝚁𝚛𝚜𝚜𝚣𝚡𝚡",
+        "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:𝙾𝚠𝚗𝚎𝚛 𝚁𝚛𝚜𝚜𝚣𝚡𝚡\nitem1.TEL;waid=6281226675327:6281226675327\nitem1.X-ABLabel:📍 Developer\nitem2.EMAIL;type=INTERNET:alanadipermana261@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://www.instagram.com/rrsszxxofc/\nitem3.X-ABLabel:⚙️ Instagram\nitem4.ADR:;;⛩️ Japan;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 FrontEnd Dev.\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
+      }]
   }, MessageType.contactsArray, { quoted: m })
 }
 handler.help = ['owner', 'creator']
